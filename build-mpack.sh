47004583c9bbbf84a4d271ee4111a9ad55c6fc4d
Lukasz <lukaszunity@users.noreply.github.com>
2015-05-27 11:29:47 +0200

    Use mdtool from local MD repository instead of Xamarin Studio mdtool.

--- build-mpack.sh
+++ build-mpack.sh
@@ -4,6 +4,6 @@ xbuild /p:Configuration=Release /t:Rebuild
 
 cd build-mpack
 
-mono "/Applications/Xamarin Studio.app/Contents/Resources/lib/monodevelop/bin/mdtool.exe" setup pack ../obj/Release/MonoDevelop.Debugger.Soft.Unity.dll ../obj/Release/UnityUtilities.dll
+mono "../../monodevelop/main/build/bin/mdtool.exe" setup pack ../obj/Release/MonoDevelop.Debugger.Soft.Unity.dll ../obj/Release/UnityUtilities.dll
 
 cd ..
