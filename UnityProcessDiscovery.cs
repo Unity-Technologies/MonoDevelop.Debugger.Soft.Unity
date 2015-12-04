699282ef04be2132aa9fcb45ffc3f67b87b7e7f3
Lukasz <lukaszunity@users.noreply.github.com>
2015-12-04 10:59:21 +0100

    Renamed GetAttachableProcesses to GetAttachableProcessesAsync and added blocking GetAttachableProcesses.

--- UnityProcessDiscovery.cs
+++ UnityProcessDiscovery.cs
@@ -16,11 +16,9 @@ namespace MonoDevelop.Debugger.Soft.Unity
 		static List<ILogger> loggers = new List<ILogger>();
 		static readonly PlayerConnection unityPlayerConnection;
 
-		#if UNITY_IOS_USB_ATTACH
 		static List<UnityProcessInfo> usbProcesses = new List<UnityProcessInfo>();
 		static bool usbProcessesFinished = true;
 		static object usbLock = new object();
-		#endif
 
 		static List<UnityProcessInfo> unityProcesses = new List<UnityProcessInfo> ();
 
@@ -73,69 +71,28 @@ namespace MonoDevelop.Debugger.Soft.Unity
 			run = false;
 		}
 
-		public static UnityProcessInfo[] GetAttachableProcesses ()
+		public static UnityProcessInfo[] GetAttachableProcessesAsync ()
 		{
-			int index = 1;
 			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
 
-			StringComparison comparison = StringComparison.OrdinalIgnoreCase;
+			// Get players
+			processes.AddRange (GetUnityPlayerProcesses (false));
 
-			if (null != unityPlayerConnection) {
-				if(Monitor.TryEnter (unityPlayerConnection)) {
-					try {
-						foreach (string player in unityPlayerConnection.AvailablePlayers) {
-							try {
-								PlayerConnection.PlayerInfo info = PlayerConnection.PlayerInfo.Parse (player);
-								if (info.m_AllowDebugging) {
-									UnityPlayers[info.m_Guid] = info;
-									processes.Add (new UnityProcessInfo (info.m_Guid, info.m_Id));
-									++index;
-								}
-							} catch {
-								// Don't care; continue
-							}
-						}
-					}
-					finally {
-						Monitor.Exit (unityPlayerConnection);
-					}
-				}
-			}
-
+			// Get editor
 			if (unityProcessesFinished) 
 			{
 				unityProcessesFinished = false;
 
-				ThreadPool.QueueUserWorkItem (delegate {
-
-					Process[] systemProcesses = Process.GetProcesses();
-					var unityThreadProcesses = new List<UnityProcessInfo>();
-
-					if(systemProcesses != null)
-					{
-						foreach (Process p in systemProcesses) {
-							try {
-								if ((p.ProcessName.StartsWith ("unity", comparison) ||
-									p.ProcessName.Contains ("Unity.app")) &&
-									!p.ProcessName.Contains ("UnityShader") &&
-									!p.ProcessName.Contains ("UnityHelper") &&
-									!p.ProcessName.Contains ("Unity Helper")) {
-									unityThreadProcesses.Add (new UnityProcessInfo (p.Id, string.Format ("{0} ({1})", "Unity Editor", p.ProcessName)));
-								}
-							} catch {
-								// Don't care; continue
-							}
-						}
-
-						unityProcesses = unityThreadProcesses;
-						unityProcessesFinished = true;
-					}
+				ThreadPool.QueueUserWorkItem (delegate 
+				{
+					unityProcesses = GetUnityEditorProcesses();
+					unityProcessesFinished = true;
 				});
 			}
 
 			processes.AddRange (unityProcesses);
 
-			#if UNITY_IOS_USB_ATTACH
+			// Get iOS USB
 			if (usbProcessesFinished)
 			{
 				usbProcessesFinished = false;
@@ -144,31 +101,111 @@ namespace MonoDevelop.Debugger.Soft.Unity
 					// Direct USB devices
 					lock(usbLock)
 					{
-						var usbThreadProcesses = new List<UnityProcessInfo>();
-
-						try
-						{
-							iOSDevices.GetUSBDevices (ConnectorRegistry, usbThreadProcesses);
-						}
-						catch(NotSupportedException)
-						{
-							Log("iOS over USB not supported on this platform");
-						}
-						catch(Exception e)
-						{
-							Log("iOS USB Error: " + e);
-						}
-						usbProcesses = usbThreadProcesses;
+						var usbThreadProcesses = GetUnityiOSUsbProcesses();
 						usbProcessesFinished = true;
 					}
 				});
 			}
 
 			processes.AddRange (usbProcesses);
-			#endif
 
 			return processes.ToArray ();
 		}
+
+		public static List<UnityProcessInfo> GetAttachableProcesses ()
+		{
+			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
+			processes.AddRange (GetUnityPlayerProcesses (true));
+			processes.AddRange (GetUnityEditorProcesses ());
+			processes.AddRange (GetUnityiOSUsbProcesses ());
+
+			return processes;
+		}
+
+		public static List<UnityProcessInfo> GetUnityEditorProcesses()
+		{
+			StringComparison comparison = StringComparison.OrdinalIgnoreCase;
+
+			Process[] systemProcesses = Process.GetProcesses();
+			var unityEditorProcesses = new List<UnityProcessInfo>();
+
+			if (systemProcesses != null) {
+				foreach (Process p in systemProcesses) {
+					try {
+						if ((p.ProcessName.StartsWith ("unity", comparison) ||
+							p.ProcessName.Contains ("Unity.app")) &&
+							!p.ProcessName.Contains ("UnityShader") &&
+							!p.ProcessName.Contains ("UnityHelper") &&
+							!p.ProcessName.Contains ("Unity Helper")) {
+							unityEditorProcesses.Add (new UnityProcessInfo (p.Id, string.Format ("{0} ({1})", "Unity Editor", p.ProcessName)));
+						}
+					} catch {
+						// Don't care; continue
+					}
+				}
+			}
+
+			return unityEditorProcesses;
+		}
+
+		public static List<UnityProcessInfo> GetUnityPlayerProcesses(bool block)
+		{
+			int index = 1;
+			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
+
+			if (null != unityPlayerConnection) 
+			{
+				if (block)
+					Monitor.Enter (unityPlayerConnection);
+				else
+					if(!Monitor.TryEnter(unityPlayerConnection))
+						return processes;
+				try 
+				{
+					foreach (string player in unityPlayerConnection.AvailablePlayers) {
+						try {
+							PlayerConnection.PlayerInfo info = PlayerConnection.PlayerInfo.Parse (player);
+							if (info.m_AllowDebugging) {
+								UnityPlayers[info.m_Guid] = info;
+								processes.Add (new UnityProcessInfo (info.m_Guid, info.m_Id));
+								++index;
+							}
+						} catch {
+							// Don't care; continue
+						}
+					}
+				}
+				finally 
+				{
+					Monitor.Exit (unityPlayerConnection);
+				}
+			}
+
+			return processes;
+		}
+
+		public static List<UnityProcessInfo> GetUnityiOSUsbProcesses()
+		{
+			var processes = new List<UnityProcessInfo>();
+
+			#if UNITY_IOS_USB_ATTACH
+			try
+			{
+				iOSDevices.GetUSBDevices (ConnectorRegistry, processes);
+			}
+			catch(NotSupportedException)
+			{
+				Log("iOS over USB not supported on this platform");
+			}
+			catch(Exception e)
+			{
+				Log("iOS USB Error: " + e);
+			}
+			#endif
+
+			return processes;
+		}
+
 	}
 
 	// Allows to define how to setup and tear down connection for debugger to connect to the
