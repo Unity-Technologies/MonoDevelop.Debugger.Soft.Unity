118b695f1ae2e6c2e88791271fc7e6482c50833f
nicklas <miniwolf1508@gmail.com>
2018-08-14 11:23:40 +0200

    Improve attaching to unity processes.
    Updated regex to support PS4, Xbox One, and Switch.
    Added additional log outputs for locating issues in future releases.

--- PlayerConnection.cs
+++ PlayerConnection.cs
@@ -1,5 +1,5 @@
 // 
-// PlayerConnection.cs 
+// PlayerConnection.cs
 //   
 // Authors:
 //       Kim Steen Riber <kim@unity3d.com>
@@ -37,146 +37,143 @@ using System.Linq;
 
 namespace MonoDevelop.Debugger.Soft.Unity
 {
-	/// <summary>
-	/// Discovery subset of native PlayerConnection class.
-	/// </summary>
-	public class PlayerConnection
-	{
-		public static readonly int[] PLAYER_MULTICAST_PORTS = new[]{ 54997, 34997, 57997, 58997 };
-		public const string PLAYER_MULTICAST_GROUP = "225.0.0.222";
-		public const int MAX_LAST_SEEN_ITERATIONS = 3;
-		
-		private List<Socket> m_MulticastSockets = null;
-		private Dictionary<string,int> m_AvailablePlayers = new Dictionary<string,int>();
-		
-		public IEnumerable<string> AvailablePlayers {
-			get {
-				return m_AvailablePlayers.Where (p => (0 < p.Value)).Select (p => p.Key);
-			}
-		}
-		
-		public struct PlayerInfo
-		{
-			public IPEndPoint m_IPEndPoint;
-			public UInt32 m_Flags;
-			public UInt32 m_Guid;
-			public UInt32 m_EditorGuid;
-			public Int32 m_Version;
-			public string m_Id;
-			public bool m_AllowDebugging;
-			public UInt32 m_DebuggerPort;
-			
-			public override string ToString ()
-			{
-				return string.Format ("PlayerInfo {0} {1} {2} {3} {4} {5} {6}:{7} {8}", m_IPEndPoint.Address, m_IPEndPoint.Port,
-									  m_Flags, m_Guid, m_EditorGuid, m_Version, m_Id, m_DebuggerPort, m_AllowDebugging? 1: 0);
-			}
-			
-			public static PlayerInfo Parse(string playerString)
-			{
-				PlayerInfo res = new PlayerInfo();
-				
-				try {
-					// "[IP] %s [Port] %u [Flags] %u [Guid] %u [EditorId] %u [Version] %d [Id] %s(:d) [Debug] %d"
-					Regex r = new Regex("\\[IP\\] (?<ip>.*) \\[Port\\] (?<port>.*) \\[Flags\\] (?<flags>.*)" +
-										" \\[Guid\\] (?<guid>.*) \\[EditorId\\] (?<editorid>.*) \\[Version\\] (?<version>.*)" +
-										" \\[Id\\] (?<id>[^:]+)(:(?<debuggerPort>\\d+))? \\[Debug\\] (?<debug>.*)");
-					
-					MatchCollection matches = r.Matches(playerString);
-					
-					if (matches.Count != 1)
-					{
-						throw new Exception(string.Format("Player string not recognised {0}", playerString));
-					}
-					
-					string ip = matches[0].Groups["ip"].Value;
-					
-					res.m_IPEndPoint = new IPEndPoint(IPAddress.Parse(ip), UInt16.Parse (matches[0].Groups["port"].Value));
-					res.m_Flags = UInt32.Parse(matches[0].Groups["flags"].Value);
-					res.m_Guid = UInt32.Parse(matches[0].Groups["guid"].Value);
-					res.m_EditorGuid = UInt32.Parse(matches[0].Groups["guid"].Value);
-					res.m_Version = Int32.Parse (matches[0].Groups["version"].Value);
-					res.m_Id = matches[0].Groups["id"].Value;
-					res.m_AllowDebugging= (0 != int.Parse (matches[0].Groups["debug"].Value));
-					if (matches[0].Groups["debuggerPort"].Success)
-						res.m_DebuggerPort = UInt32.Parse (matches[0].Groups["debuggerPort"].Value);
-					
-					System.Console.WriteLine(res.ToString());
-				} catch (Exception e) {
-					throw new ArgumentException ("Unable to parse player string", e);
-				}
-				
-				return res;
-			}
-		}
-		
-		public PlayerConnection ()
-		{
-			m_MulticastSockets = new List<Socket> ();
-			NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
-			foreach (NetworkInterface adapter in nics)
-			{
-				if (adapter.Supports(NetworkInterfaceComponent.IPv4) == false)
-				{
-					continue;
-				}
+    /// <summary>
+    /// Discovery subset of native PlayerConnection class.
+    /// </summary>
+    public class PlayerConnection
+    {
+        static readonly int[] PLAYER_MULTICAST_PORTS = { 54997, 34997, 57997, 58997 };
+        const string PLAYER_MULTICAST_GROUP = "225.0.0.222";
+        const int MAX_LAST_SEEN_ITERATIONS = 3;
 
-				//Fetching adapter index
-				IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
-				IPv4InterfaceProperties p = adapterProperties.GetIPv4Properties();
+        List<Socket> m_MulticastSockets;
+        Dictionary<string, int> m_AvailablePlayers = new Dictionary<string, int>();
 
-				foreach (int port in PLAYER_MULTICAST_PORTS)
-				{
-					try
-					{
-						var multicastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-						try { multicastSocket.ExclusiveAddressUse = false; }
-						catch (SocketException)
-						{
-							// This option is not supported on some OSs
-						}
-						multicastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-						IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port);
-						multicastSocket.Bind(ipep);
+        public IEnumerable<string> AvailablePlayers
+        {
+            get { return m_AvailablePlayers.Where(p => (0 < p.Value)).Select(p => p.Key); }
+        }
 
-						IPAddress ip = IPAddress.Parse(PLAYER_MULTICAST_GROUP);
-						multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
-											new MulticastOption(ip, p.Index));
-						m_MulticastSockets.Add(multicastSocket);
-					}
-					catch
-					{
-						throw;
-					}
-				}
-			}
-		}
-		
-		public void Poll ()
-		{
-			// Update last-seen
-			foreach (string player in m_AvailablePlayers.Keys.ToList ()) {
-				--m_AvailablePlayers[player];
-			}
+        public struct PlayerInfo
+        {
+            public IPEndPoint m_IPEndPoint;
+            public UInt32 m_Flags;
+            public UInt32 m_Guid;
+            public UInt32 m_EditorGuid;
+            public Int32 m_Version;
+            public string m_Id;
+            public bool m_AllowDebugging;
+            public UInt32 m_DebuggerPort;
 
-			foreach (Socket socket in m_MulticastSockets)
-			{
-				while (socket != null && socket.Available > 0)
-				{
-					byte[] buffer = new byte[1024];
+            public override string ToString()
+            {
+                return $"PlayerInfo {m_IPEndPoint.Address} {m_IPEndPoint.Port} {m_Flags} {m_Guid} {m_EditorGuid}" +
+                    $" {m_Version} {m_Id}:{m_DebuggerPort} {(m_AllowDebugging ? 1 : 0)}";
+            }
 
-					int num = socket.Receive(buffer);
-					string str = System.Text.Encoding.ASCII.GetString(buffer, 0, num);
+            public static PlayerInfo Parse(string playerString)
+            {
+                var res = new PlayerInfo();
 
-					RegisterPlayer(str);
-				}
-			}
-		}
-		
-		protected void RegisterPlayer(string playerString)
-		{
-			m_AvailablePlayers[playerString] = MAX_LAST_SEEN_ITERATIONS;
-		}
-	}
+                try
+                {
+                    // "[IP] %s [Port] %u [Flags] %u [Guid] %u [EditorId] %u [Version] %d [Id] %s(:d) [Debug] %d [PackageName] %s"
+                    var r = new Regex("\\[IP\\] (?<ip>.*) \\[Port\\] (?<port>.*) \\[Flags\\] (?<flags>.*)" +
+                        " \\[Guid\\] (?<guid>.*) \\[EditorId\\] (?<editorid>.*) \\[Version\\] (?<version>.*)" +
+                        " \\[Id\\] (?<id>[^:]+)(:(?<debuggerPort>\\d+))? \\[Debug\\] (?<debug>.*)" +
+                        " \\[PackageName\\] (?<packageName>.*)");
+
+                    var matches = r.Matches(playerString);
+
+                    if (matches.Count != 1)
+                    {
+                        throw new Exception($"Player string not recognised {playerString}");
+                    }
+
+                    var ip = matches[0].Groups["ip"].Value;
+
+                    res.m_IPEndPoint = new IPEndPoint(IPAddress.Parse(ip), UInt16.Parse(matches[0].Groups["port"].Value));
+                    res.m_Flags = uint.Parse(matches[0].Groups["flags"].Value);
+                    res.m_Guid = uint.Parse(matches[0].Groups["guid"].Value);
+                    res.m_EditorGuid = uint.Parse(matches[0].Groups["guid"].Value);
+                    res.m_Version = int.Parse(matches[0].Groups["version"].Value);
+                    res.m_Id = matches[0].Groups["id"].Value;
+                    res.m_AllowDebugging = 0 != int.Parse(matches[0].Groups["debug"].Value);
+                    if (matches[0].Groups["debuggerPort"].Success)
+                        res.m_DebuggerPort = uint.Parse(matches[0].Groups["debuggerPort"].Value);
+
+                    Console.WriteLine(res.ToString());
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Unable to parse player string", e);
+                }
+
+                return res;
+            }
+        }
+
+        public PlayerConnection()
+        {
+            m_MulticastSockets = new List<Socket>();
+            var nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4) == false)
+                {
+                    continue;
+                }
+
+                //Fetching adapter index
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                IPv4InterfaceProperties p = adapterProperties.GetIPv4Properties();
+
+                foreach (var port in PLAYER_MULTICAST_PORTS)
+                {
+                    var multicastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    try { multicastSocket.ExclusiveAddressUse = false; }
+                    catch (SocketException)
+                    {
+                        // This option is not supported on some OSs
+                    }
+
+                    multicastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    var ipep = new IPEndPoint(IPAddress.Any, port);
+                    multicastSocket.Bind(ipep);
+
+                    var ip = IPAddress.Parse(PLAYER_MULTICAST_GROUP);
+                    multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
+                        new MulticastOption(ip, p.Index));
+                    m_MulticastSockets.Add(multicastSocket);
+                }
+            }
+        }
+
+        public void Poll()
+        {
+            // Update last-seen
+            foreach (var player in m_AvailablePlayers.Keys.ToList())
+            {
+                --m_AvailablePlayers[player];
+            }
+
+            foreach (Socket socket in m_MulticastSockets)
+            {
+                while (socket != null && socket.Available > 0)
+                {
+                    var buffer = new byte[1024];
+
+                    var num = socket.Receive(buffer);
+                    var str = System.Text.Encoding.ASCII.GetString(buffer, 0, num);
+
+                    RegisterPlayer(str);
+                }
+            }
+        }
+
+        void RegisterPlayer(string playerString)
+        {
+            m_AvailablePlayers[playerString] = MAX_LAST_SEEN_ITERATIONS;
+        }
+    }
 }
-
--- UnityProcessDiscovery.cs
+++ UnityProcessDiscovery.cs
@@ -7,150 +7,160 @@ using System.Linq;
 
 namespace MonoDevelop.Debugger.Soft.Unity
 {
-	public static class UnityProcessDiscovery
-	{
-		[Flags]
-		public enum GetProcessOptions
-		{
-			Editor = (1 << 0),
-			Players = (1 << 1),
-			All = Editor | Players
-		};
+    public static class UnityProcessDiscovery
+    {
+        [Flags]
+        public enum GetProcessOptions
+        {
+            Editor = (1 << 0),
+            Players = (1 << 1),
+            All = Editor | Players
+        };
 
-		static readonly PlayerConnection unityPlayerConnection;
+        static readonly PlayerConnection unityPlayerConnection;
 
-		static List<UnityProcessInfo> usbProcesses = new List<UnityProcessInfo>();
-		static bool usbProcessesFinished = true;
-		static object usbLock = new object();
+        static List<UnityProcessInfo> usbProcesses = new List<UnityProcessInfo>();
+        static bool usbProcessesFinished = true;
+        static object usbLock = new object();
 
-		static List<UnityProcessInfo> unityProcesses = new List<UnityProcessInfo> ();
+        static List<UnityProcessInfo> unityProcesses = new List<UnityProcessInfo>();
 
-		static bool unityProcessesFinished = true;
-		static bool run = true;
+        static bool unityProcessesFinished = true;
+        static bool run = true;
 
-		internal static Dictionary<uint, PlayerConnection.PlayerInfo> UnityPlayers {
-			get;
-			private set;
-		}
+        internal static Dictionary<uint, PlayerConnection.PlayerInfo> UnityPlayers { get; private set; }
 
-		internal static ConnectorRegistry ConnectorRegistry { get; private set; }
+        internal static ConnectorRegistry ConnectorRegistry { get; private set; }
 
-		static UnityProcessDiscovery ()
-		{
-			UnityPlayers = new Dictionary<uint, PlayerConnection.PlayerInfo> ();
-			ConnectorRegistry = new ConnectorRegistry();
+        static UnityProcessDiscovery()
+        {
+            UnityPlayers = new Dictionary<uint, PlayerConnection.PlayerInfo>();
+            ConnectorRegistry = new ConnectorRegistry();
 
-			try {
-				// HACK: Poll Unity players
-				unityPlayerConnection = new PlayerConnection ();
-				ThreadPool.QueueUserWorkItem (delegate {
-					while (run) {
-						lock (unityPlayerConnection) {
-							unityPlayerConnection.Poll ();
-						}
-						Thread.Sleep (1000);
-					}
-				});
-			} 
-			catch (Exception e)
-			{
-				Log.Error ("Error launching player connection discovery service: Unity player discovery will be unavailable", e);
-			}
-		}
+            try
+            {
+                // HACK: Poll Unity players
+                unityPlayerConnection = new PlayerConnection();
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    while (run)
+                    {
+                        lock (unityPlayerConnection)
+                        {
+                            unityPlayerConnection.Poll();
+                        }
 
-		public static void Stop()
-		{
-			run = false;
-		}
+                        Thread.Sleep(1000);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error launching player connection discovery service: Unity player discovery will be unavailable", e);
+            }
+        }
 
-		public static UnityAttachInfo GetUnityAttachInfo(long processId, ref IUnityDbgConnector connector)
-		{
-			if (ConnectorRegistry.Connectors.ContainsKey((uint)processId)) {
-				connector = ConnectorRegistry.Connectors[(uint)processId];
-				return connector.SetupConnection();
-			} else if (UnityProcessDiscovery.UnityPlayers.ContainsKey ((uint)processId)) {
-				PlayerConnection.PlayerInfo player = UnityProcessDiscovery.UnityPlayers[(uint)processId];
-				int port = (0 == player.m_DebuggerPort
-					? (int)(56000 + (processId % 1000))
-					: (int)player.m_DebuggerPort);
-				try {
-					return new UnityAttachInfo (player.m_Id, player.m_IPEndPoint.Address, (int)port);
-				} catch (Exception ex) {
-					throw new Exception (string.Format ("Unable to attach to {0}:{1}", player.m_IPEndPoint.Address, port), ex);
-				}
-			}
+        public static void Stop()
+        {
+            run = false;
+        }
 
-			long defaultPort = 56000 + (processId % 1000);
-			return new UnityAttachInfo (null, IPAddress.Loopback, (int)defaultPort);
-		}
+        public static UnityAttachInfo GetUnityAttachInfo(long processId, ref IUnityDbgConnector connector)
+        {
+            if (ConnectorRegistry.Connectors.ContainsKey((uint)processId))
+            {
+                connector = ConnectorRegistry.Connectors[(uint)processId];
+                return connector.SetupConnection();
+            }
+            else if (UnityPlayers.ContainsKey((uint)processId))
+            {
+                PlayerConnection.PlayerInfo player = UnityPlayers[(uint)processId];
+                int port = 0 == player.m_DebuggerPort
+                    ? (int)(56000 + processId % 1000)
+                    : (int)player.m_DebuggerPort;
+                try
+                {
+                    return new UnityAttachInfo(player.m_Id, player.m_IPEndPoint.Address, port);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Unable to attach to {player.m_IPEndPoint.Address}:{port}", ex);
+                }
+            }
 
-		public static UnityProcessInfo[] GetAttachableProcessesAsync ()
-		{
-			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
+            long defaultPort = 56000 + (processId % 1000);
+            return new UnityAttachInfo(null, IPAddress.Loopback, (int)defaultPort);
+        }
 
-			// Get players
-			processes.AddRange (GetUnityPlayerProcesses (false));
+        /*public static UnityProcessInfo[] GetAttachableProcessesAsync()
+        {
+            List<UnityProcessInfo> processes = new List<UnityProcessInfo>();
 
-			// Get editor
-			if (unityProcessesFinished) 
-			{
-				unityProcessesFinished = false;
+            // Get players
+            processes.AddRange(GetUnityPlayerProcesses(false));
 
-				ThreadPool.QueueUserWorkItem (delegate 
-				{
-					unityProcesses = GetUnityEditorProcesses();
-					unityProcessesFinished = true;
-				});
-			}
+            // Get editor
+            if (unityProcessesFinished)
+            {
+                unityProcessesFinished = false;
 
-			processes.AddRange (unityProcesses);
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    unityProcesses = GetUnityEditorProcesses();
+                    unityProcessesFinished = true;
+                });
+            }
 
-			// Get iOS USB
-			if (usbProcessesFinished)
-			{
-				usbProcessesFinished = false;
+            processes.AddRange(unityProcesses);
 
-				ThreadPool.QueueUserWorkItem (delegate {
-					// Direct USB devices
-					lock(usbLock)
-					{
-						usbProcesses = GetUnityiOSUsbProcesses();
-						usbProcessesFinished = true;
-					}
-				});
-			}
+            // Get iOS USB
+            if (usbProcessesFinished)
+            {
+                usbProcessesFinished = false;
 
-			processes.AddRange (usbProcesses);
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    // Direct USB devices
+                    lock (usbLock)
+                    {
+                        usbProcesses = GetUnityiOSUsbProcesses();
+                        usbProcessesFinished = true;
+                    }
+                });
+            }
 
-			return processes.ToArray ();
-		}
+            processes.AddRange(usbProcesses);
 
-		public static List<UnityProcessInfo> GetAttachableProcesses (GetProcessOptions options = GetProcessOptions.All)
-		{
-			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
+            return processes.ToArray();
+        }*/
 
-			if((options & GetProcessOptions.Editor) == GetProcessOptions.Editor)
-				processes.AddRange (GetUnityEditorProcesses ());
-	
-			if ((options & GetProcessOptions.Players) == GetProcessOptions.Players) {
-				processes.AddRange (GetUnityPlayerProcesses (true));
-				processes.AddRange (GetUnityiOSUsbProcesses ());
-			}
+        public static List<UnityProcessInfo> GetAttachableProcesses(GetProcessOptions options = GetProcessOptions.All)
+        {
+            List<UnityProcessInfo> processes = new List<UnityProcessInfo>();
 
-			return processes;
-		}
+            if ((options & GetProcessOptions.Editor) == GetProcessOptions.Editor)
+                processes.AddRange(GetUnityEditorProcesses());
 
-		public static List<UnityProcessInfo> GetUnityEditorProcesses()
-		{
+            if ((options & GetProcessOptions.Players) == GetProcessOptions.Players)
+            {
+                processes.AddRange(GetUnityPlayerProcesses(true));
+                processes.AddRange(GetUnityiOSUsbProcesses());
+            }
+
+            return processes;
+        }
+
+        public static List<UnityProcessInfo> GetUnityEditorProcesses()
+        {
             var unityEditorProcessNames = new string[] { "Unity", "Unity Editor" };
 
-			Process[] systemProcesses = Process.GetProcesses();
-			var unityEditorProcesses = new List<UnityProcessInfo>();
+            Process[] systemProcesses = Process.GetProcesses();
+            var unityEditorProcesses = new List<UnityProcessInfo>();
 
             if (systemProcesses == null)
                 return unityEditorProcesses;
-            
-	        foreach (Process p in systemProcesses) 
+
+            foreach (Process p in systemProcesses)
             {
                 try
                 {
@@ -166,129 +176,131 @@ namespace MonoDevelop.Debugger.Soft.Unity
                 {
                     // Don't care; continue
                 }
-			}
+            }
 
-			return unityEditorProcesses;
-		}
+            return unityEditorProcesses;
+        }
 
-		public static List<UnityProcessInfo> GetUnityPlayerProcesses(bool block)
-		{
-			int index = 1;
-			List<UnityProcessInfo> processes = new List<UnityProcessInfo> ();
+        public static List<UnityProcessInfo> GetUnityPlayerProcesses(bool block)
+        {
+            int index = 1;
+            List<UnityProcessInfo> processes = new List<UnityProcessInfo>();
 
-			if (null != unityPlayerConnection) 
-			{
-				if (block)
-				{
-					Monitor.Enter (unityPlayerConnection);
+            if (null != unityPlayerConnection)
+            {
+                if (block)
+                {
+                    Monitor.Enter(unityPlayerConnection);
 
-					for (int i = 0; i < 12 && !unityPlayerConnection.AvailablePlayers.Any (); ++i) {
-						unityPlayerConnection.Poll ();
-						Thread.Sleep (250);
-					}
-				}
-				else
-					if(!Monitor.TryEnter(unityPlayerConnection))
-						return processes;
-				try 
-				{
-					foreach (string player in unityPlayerConnection.AvailablePlayers) {
-						try {
-							PlayerConnection.PlayerInfo info = PlayerConnection.PlayerInfo.Parse (player);
-							if (info.m_AllowDebugging) {
-								UnityPlayers[info.m_Guid] = info;
-								processes.Add (new UnityProcessInfo (info.m_Guid, info.m_Id));
-								++index;
-							}
-						} catch {
-							// Don't care; continue
-						}
-					}
-				}
-				finally 
-				{
-					Monitor.Exit (unityPlayerConnection);
-				}
-			}
+                    for (int i = 0; i < 12 && !unityPlayerConnection.AvailablePlayers.Any(); ++i)
+                    {
+                        unityPlayerConnection.Poll();
+                        Thread.Sleep(250);
+                    }
+                }
+                else if (!Monitor.TryEnter(unityPlayerConnection))
+                    return processes;
 
-			return processes;
-		}
+                try
+                {
+                    foreach (string player in unityPlayerConnection.AvailablePlayers)
+                    {
+                        try
+                        {
+                            PlayerConnection.PlayerInfo info = PlayerConnection.PlayerInfo.Parse(player);
+                            if (info.m_AllowDebugging)
+                            {
+                                UnityPlayers[info.m_Guid] = info;
+                                processes.Add(new UnityProcessInfo(info.m_Guid, info.m_Id));
+                                ++index;
+                            }
+                        }
+                        catch
+                        {
+                            // Don't care; continue
+                        }
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(unityPlayerConnection);
+                }
+            }
 
-		public static List<UnityProcessInfo> GetUnityiOSUsbProcesses()
-		{
-			var processes = new List<UnityProcessInfo>();
+            return processes;
+        }
 
-			try
-			{
-				iOSDevices.GetUSBDevices (ConnectorRegistry, processes);
-			}
-			catch(NotSupportedException)
-			{
-				Log.Info("iOS over USB not supported on this platform");
-			}
-			catch(Exception e)
-			{
-				Log.Info("iOS USB Error: " + e);
-			}
+        public static List<UnityProcessInfo> GetUnityiOSUsbProcesses()
+        {
+            var processes = new List<UnityProcessInfo>();
 
-			return processes;
-		}
+            try
+            {
+                iOSDevices.GetUSBDevices(ConnectorRegistry, processes);
+            }
+            catch (NotSupportedException)
+            {
+                Log.Info("iOS over USB not supported on this platform");
+            }
+            catch (Exception e)
+            {
+                Log.Info("iOS USB Error: " + e);
+            }
 
-	}
+            return processes;
+        }
+    }
 
-	// Allows to define how to setup and tear down connection for debugger to connect to the
-	// debugee. For example to setup TCP tunneling over USB.
-	public interface IUnityDbgConnector
-	{
-		UnityAttachInfo SetupConnection();
-		void OnDisconnect();
-	}
+    // Allows to define how to setup and tear down connection for debugger to connect to the
+    // debugee. For example to setup TCP tunneling over USB.
+    public interface IUnityDbgConnector
+    {
+        UnityAttachInfo SetupConnection();
+        void OnDisconnect();
+    }
 
-	public class ConnectorRegistry
-	{
-		// This is used to map process id <-> unique string id. MonoDevelop attachment is built
-		// around process ids.
-		object processIdLock = new object();
-		uint nextProcessId = 1000000;
-		Dictionary<uint, string> processIdToUniqueId = new Dictionary<uint, string>();
-		Dictionary<string, uint> uniqueIdToProcessId = new Dictionary<string, uint>();
+    public class ConnectorRegistry
+    {
+        // This is used to map process id <-> unique string id. MonoDevelop attachment is built
+        // around process ids.
+        object processIdLock = new object();
+        uint nextProcessId = 1000000;
+        Dictionary<uint, string> processIdToUniqueId = new Dictionary<uint, string>();
+        Dictionary<string, uint> uniqueIdToProcessId = new Dictionary<string, uint>();
 
-		public Dictionary<uint, IUnityDbgConnector> Connectors { get; private set; }
+        public Dictionary<uint, IUnityDbgConnector> Connectors { get; private set; }
 
+        public uint GetProcessIdForUniqueId(string uid)
+        {
+            lock (processIdLock)
+            {
+                uint processId;
+                if (uniqueIdToProcessId.TryGetValue(uid, out processId))
+                    return processId;
 
-		public uint GetProcessIdForUniqueId(string uid)
-		{
-			lock (processIdLock)
-			{
-				uint processId;
-				if (uniqueIdToProcessId.TryGetValue(uid, out processId))
-					return processId;
+                processId = nextProcessId++;
+                processIdToUniqueId.Add(processId, uid);
+                uniqueIdToProcessId.Add(uid, processId);
 
-				processId = nextProcessId++;
-				processIdToUniqueId.Add(processId, uid);
-				uniqueIdToProcessId.Add(uid, processId);
+                return processId;
+            }
+        }
 
-				return processId;
-			}
-		}
+        public string GetUniqueIdFromProcessId(uint processId)
+        {
+            lock (processIdLock)
+            {
+                string uid;
+                if (processIdToUniqueId.TryGetValue(processId, out uid))
+                    return uid;
 
+                return null;
+            }
+        }
 
-		public string GetUniqueIdFromProcessId(uint processId)
-		{
-			lock (processIdLock) {
-				string uid;
-				if (processIdToUniqueId.TryGetValue(processId, out uid))
-					return uid;
-
-				return null;
-			}
-		}
-
-
-		public ConnectorRegistry()
-		{
-			Connectors = new Dictionary<uint, IUnityDbgConnector>();
-		}
-	}
+        public ConnectorRegistry()
+        {
+            Connectors = new Dictionary<uint, IUnityDbgConnector>();
+        }
+    }
 }
-
